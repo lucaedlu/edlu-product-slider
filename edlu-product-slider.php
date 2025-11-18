5365ccadd591d3a32fa0f31e151e0249867c1465
Luca Falcone <luca.edlu@gmail.com>
2025-11-18 01:31:21 +0100

    Update edlu-product-slider.php

--- edlu-product-slider.php
+++ edlu-product-slider.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Product Slider Elementor
  * Description: Widget Elementor per mostrare prodotti WooCommerce in griglia/slider.
- * Version: 0.7
+ * Version: 0.8
  * Author: EDLU Digital Services
  */
 
@@ -53,14 +53,14 @@ function edlu_ps_enqueue_assets() {
         'edlu-product-slider-css',
         plugin_dir_url( __FILE__ ) . 'edlu-product-slider.css',
         array(),
-        '0.7'
+        '0.8'
     );
 
     wp_enqueue_script(
         'edlu-product-slider-js',
         plugin_dir_url( __FILE__ ) . 'edlu-product-slider.js',
         array( 'jquery' ),
-        '0.7',
+        '0.8',
         true
     );
 }
